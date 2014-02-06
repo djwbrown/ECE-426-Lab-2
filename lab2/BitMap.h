f6139192894b4b0bab03d5f005bd22e0d9eacd68
Dylan Brown <djwbrown@gmail.com>
2014-02-06 15:58:45 -0800

    Separated BitMap class to header and implementation files.

---
+++ lab2/BitMap.h
@@ -0,0 +1,28 @@
+//
+//  BitMap.h
+//  lab2
+//
+//  Created by Dylan Brown on 2/6/14.
+//  Copyright (c) 2014 Dylan Brown. All rights reserved.
+//
+
+#ifndef __lab2__BitMap__
+#define __lab2__BitMap__
+
+#include <iostream>
+
+#endif /* defined(__lab2__BitMap__) */
+
+#define N 1024
+
+class BitMap {
+public:
+    BitMap();
+    bool search(int n, unsigned int blocks[]);
+    void freeBlocks(int n, unsigned int blocks[]);
+    bool searchConsecutive(int n,
+                           unsigned int startAddress,
+                           unsigned int blockAddress);
+private:
+    unsigned char bitmap[N];
+};
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -7,56 +7,7 @@
 //
 
 #include <iostream>
-
-#define N 1024
-
-class BitMap {
-    
-public:
-    BitMap();
-    bool search(int n, unsigned int blocks[]);
-    void freeBlocks(int n, unsigned int blocks[]);
-    bool searchConsecutive(int n,
-                           unsigned int startAddress,
-                           unsigned int blockAddress);
-    
-private:
-    unsigned char bitmap[N];
-};
-
-BitMap::BitMap(void) {
-    
-    /* Initialize the bitmap with random binary values */
-    srand((unsigned int)time(NULL));
-    for (int i = 0; i < N; i++) {
-        bitmap[i] = rand()%256;
-    }
-    
-    /* Reset the bitmap[0][0] address. Mark as used. */
-    bitmap[0] = bitmap[0] & 0xFE;
-}
-
-bool BitMap::search(int n, unsigned int blocks[]) {
-
-    unsigned char testBit;
-    int address;
-    int k = 0;
-    
-    /* Find the free blocks by testing each address in the bitmap */
-    for (int i = 0; i < N; i++) {
-        testBit = 0x01;
-        for (int j = 0; j < 8; j++) {
-            address = i * 8 + j;
-            if (address > (N * 8)) return false;
-            if ((bitmap[i] & testBit) > 0) {
-                blocks[k++] = address;
-                if (--n == 0) return true;
-            }
-            testBit = testBit << 1;
-        }
-    }
-    return false;
-}
+#include "BitMap.h"
 
 int main(int argc, const char * argv[])
 {
